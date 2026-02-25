9d9e52f83aee6f2947e50e84058f2d5a8b79c8da
isabela <isasierra225@gmail.com>
2026-02-25 09:50:35 -0500

    deleting html practice

--- obj/Debug/net10.0-windows/MainWindow.g.cs
+++ obj/Debug/net10.0-windows/MainWindow.g.cs
@@ -51,7 +51,7 @@ namespace Pawdopt {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/PawdoptWpf;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/PawdoptWpf;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
